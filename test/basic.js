380c8757a1171ca70edfe2e78e9feade77f46031
Kenan Yildirim <kenan@kenany.me>
2016-10-23 17:52:45 -0400

    test: rename `test/index.js`
    Closes https://github.com/KenanY/eslint-config/issues/34

---
+++ test/basic.js
@@ -0,0 +1,22 @@
+var eslintconfig = require('../');
+var test = require('tape');
+var isPlainObject = require('lodash.isplainobject');
+var eslint = require('eslint');
+
+test('exports an object', function(t) {
+  t.plan(2);
+  t.ok(isPlainObject(eslintconfig));
+  t.ok(isPlainObject(eslintconfig.rules));
+});
+
+test('valid rule syntax', function(t) {
+  t.plan(1);
+
+  var cli = new eslint.CLIEngine({
+    useEslintrc: false,
+    configFile: '.eslintrc.js'
+  });
+
+  var code = 'var foo = 1;\nvar bar = function() {};\nbar(foo);\n';
+  t.equal(cli.executeOnText(code).errorCount, 0);
+});
--- test/index.js
+++
@@ -1,8 +0,0 @@
-var eslintconfig = require('../');
-var test = require('tape');
-var isPlainObject = require('lodash.isplainobject');
-
-test('exports an object', function(t) {
-  t.plan(1);
-  t.ok(isPlainObject(eslintconfig));
-});
